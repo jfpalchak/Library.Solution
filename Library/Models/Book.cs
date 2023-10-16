4dce6ba541a5f2cae84874c1c4325a69eef2977f
Joey Palchak <joey.palchak@gmail.com>
2023-10-16 14:40:36 -0700

    create Initial migration

--- Library/Models/Book.cs
+++ Library/Models/Book.cs
@@ -1,10 +1,12 @@
+using System;
+using System.Collections.Generic;
 
 namespace Library.Models
 {
   public class Book
   {
     public int BookId { get; set; }
-    public string Book { get; set; }
+    public string Title { get; set; }
 
     // Collection Navigation Property (For Many-to-Many Relationship with Author)
     public List<AuthorBook> JoinEntities { get; set; }
